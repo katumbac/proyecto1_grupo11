c48980c831640e2d048b46d74587aa73f8334507
rocaenca <rocaenca@espol.edu.ec>
2021-11-06 18:10:46 -0500

    Creación de la clase padre medidor y la clase Plan

---
+++ src/main/java/com/mycompany/proyecto1_grupo11/Medidor.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto1_grupo11;
+
+/**
+ *
+ * @author ROBERTO
+ */
+public class Medidor {
+    private String codigo;
+    private String provincia;
+    private double costoKwh;
+    private Plan planContratado;
+    
+    public void calcularConsumo(){}
+}
---
+++ src/main/java/com/mycompany/proyecto1_grupo11/Plan.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto1_grupo11;
+import java.util.ArrayList;
+/**
+ *
+ * @author ROBERTO
+ */
+public class Plan {
+    private String nombre;
+    private double costoKwh;
+    private ArrayList provincias;
+    private double cargoBase;
+    private String horaPico;
+    
+    public Plan(String nombre, double costoKwh, ArrayList provincias){
+        this.nombre = nombre;
+        this.costoKwh = costoKwh;
+        this.provincias = provincias;
+        this.cargoBase = 0;
+    }
+    public Plan(String nombre, double costoKwh, ArrayList provincias, double cargoBase, String horaPico){
+        this.nombre = nombre;
+        this.costoKwh = costoKwh;
+        this.provincias = provincias;
+        this.cargoBase = cargoBase;
+        this.horaPico = horaPico;
+    }
+    
+}
