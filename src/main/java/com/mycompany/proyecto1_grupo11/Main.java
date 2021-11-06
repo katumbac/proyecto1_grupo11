7611267788b7c6cd281117cf37352d5c877b2c0f
katumbac <katumbac@espol.edu.ec>
2021-11-06 12:33:47 -0500

    Creación de la Clase Main

---
+++ src/main/java/com/mycompany/proyecto1_grupo11/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto1_grupo11;
+
+/**
+ *
+ * @author user
+ */
+public class Main {
+   
+    public static void main(String[] args){
+        String mensajes = "EJEMPLO 1";
+        
+        System.out.println(mensajes);
+                
+    }
+}
