b1837481e73c51134c616d51096b70db090b17dc
KevinVargas1999 <kevvbena@espol.edu.ec>
2021-11-06 16:08:08 -0500

    creacion de clase usuarioOperario

--- src/main/java/com/mycompany/proyecto1_grupo11/usuarioOperario.java
+++ src/main/java/com/mycompany/proyecto1_grupo11/usuarioOperario.java
@@ -8,6 +8,23 @@ package com.mycompany.proyecto1_grupo11;
  *
  * @author Kevin Vargas
  */
-public class usuarioOperario {
-    
+public class usuarioOperario extends Usuario {
+    private String codigoMedidor;
+    private double lecturaAnterior;
+    private double lecturaActual;
+    private double kilovatiosConsumidos;
+
+    public usuarioOperario(String codigoMedidor, double lecturaAnterior, double lecturaActual, double kilovatiosConsumidos, String nombre, String contrasenia) {
+        super(nombre, contrasenia);
+        this.codigoMedidor = codigoMedidor;
+        this.lecturaAnterior = lecturaAnterior;
+        this.lecturaActual = lecturaActual;
+        this.kilovatiosConsumidos = kilovatiosConsumidos;
+    }
+    /**
+    public void registrarMedicion(String codigoMedidor, double lecturaActual){
+        
+    }
+    **/
+   
 }
