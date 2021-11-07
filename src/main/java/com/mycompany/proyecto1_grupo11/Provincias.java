4e9c1dc3d65e7dafe85fff0979e7ac415424ed0a
katumbac <katumbac@espol.edu.ec>
2021-11-07 00:15:31 -0500

    Creación tipo de datos enum para las provincias

--- src/main/java/com/mycompany/proyecto1_grupo11/Provincias.java
+++ src/main/java/com/mycompany/proyecto1_grupo11/Provincias.java
@@ -9,5 +9,5 @@ package com.mycompany.proyecto1_grupo11;
  * @author user
  */
 public enum Provincias {
-    
+    El_Oro,Esmeraldas,Galápagos,Guayas,Imbabura,Loja,Los_Ríos,Manabí,Morona_Santiago,Napo,Orellana, Pastaza, Pichincha,Santa_Elena,Santo_Domingo_de_los_Tsáchilas,Santo_Domingo,Sucumbíos,Tungurahua,Zamora_Chinchipe;
 }
