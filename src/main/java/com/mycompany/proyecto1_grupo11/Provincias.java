951ecc4ec0b03530748fcc76606af15619655fbd
katumbac <katumbac@espol.edu.ec>
2021-11-07 00:13:06 -0500

    Creación tipo de datos enum para las provincias

---
+++ src/main/java/com/mycompany/proyecto1_grupo11/Provincias.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.mycompany.proyecto1_grupo11;
+
+/**
+ *
+ * @author user
+ */
+public enum Provincias {
+    
+}
