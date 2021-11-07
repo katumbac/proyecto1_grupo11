d60638c68052b928ad32a41bab5586f2047294aa
katumbac <katumbac@espol.edu.ec>
2021-11-07 00:51:13 -0500

    Creación clases medidorinteligente

---
+++ src/main/java/com/mycompany/proyecto1_grupo11/MedidorInteligente.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto1_grupo11;
+
+/**
+ *
+ * @author user
+ */
+
+//import com.mycompany.proyecto1_grupo11.Medidor;
+public class MedidorInteligente extends Medidor {
+    public int horaspico;
+    public String provincia;
+    public double telemetría;
+    
+    
+    public MedidorInteligente(double telemetría, String horaspico){
+        //Costo varia en las horas pico
+    }
+    /**
+    public void setMedidorInteligente(){
+      
+    }
+
+    public int getsetMedidorInteligente(){
+      
+    }*/
+}
